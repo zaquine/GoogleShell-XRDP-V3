44ca004044108b2657231758e15b66ee343c2fef
Ryo Zaquine <92768269+zaquine@users.noreply.github.com>
2022-02-24 23:10:44 +0700

    Create ngrok.sh

---
+++ ngrok.sh
@@ -0,0 +1,7 @@
+rm -fr ngrok ngrok.sh
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
+rm -fr ngrok.zip > /dev/null 2>&1
+echo "======================"
+echo "DOWNLOADING NGROK DONE"
+echo "======================"

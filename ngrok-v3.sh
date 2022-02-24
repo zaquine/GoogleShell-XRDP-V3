9ead6f05ebd62abdef8e01ee3f59417ac8fbbc7c
Ryo Zaquine <92768269+zaquine@users.noreply.github.com>
2022-02-25 01:40:03 +0700

    Update ngrok-v3.sh

--- ngrok-v3.sh
+++ ngrok-v3.sh
@@ -1,5 +1,8 @@
+#Script V3
+#Downloading Ngrok
+
 rm -fr ngrok ngrok.sh
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
 rm -fr ngrok.zip > /dev/null 2>&1
-echo "Info: Downloading Ngrok Success..."
+echo "[Info]: Downloading Ngrok Success..."

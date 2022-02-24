7c3942819df64ec0c24180dfce4573dbbf85e763
Ryo Zaquine <92768269+zaquine@users.noreply.github.com>
2022-02-25 01:38:29 +0700

    Update build-xrdp-v3.sh

--- build-xrdp-v3.sh
+++ build-xrdp-v3.sh
@@ -3,14 +3,11 @@
 #Firefox Installed
 #Chrome Installed
 #Created by: Bintang Zaquine
-
 rm -fr google-xrdp-v2.sh
-echo "Info: PROGRESS TIME : 5 MINUTE"
-echo "Info: PLEASE USE STABIL CONNECTION"
+echo "USE STABIL CONNECTION DURING INSTALATION"
 echo ""
-echo "Running Progress....."
+echo "Progress Runing...Please Wait for 5 minute..."
 echo ""
-nohup ./ngrok tcp --region ap 3389 &>/dev/null &
 sudo apt install lxde > /dev/null 2>&1
 sudo apt install -y xrdp > /dev/null 2>&1
 sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
@@ -19,22 +16,23 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F >
 sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main" > /dev/null 2>&1
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get install firefox -y > /dev/null 2>&1
-echo "Info: INSTALLING FIREFOX SUCCESS"
+echo "[Info]: INSTALLING FIREFOX SUCCESS"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
 sudo dpkg --install google-chrome-stable_current_amd64.deb > /dev/null 2>&1
 sudo apt-get install --assume-yes --fix-broken > /dev/null 2>&1
 echo "Info: INSTALLING CHROME SUCCESS"
 echo ""
-echo "Info: FINISHING PROGRESS......."
+echo "[Info]: FINISHING PROGRESS......."
 echo ""
+nohup ./ngrok tcp --region ap 3389 &>/dev/null &
 sudo service xrdp start > /dev/null 2>&1
-echo "______________________"
-echo "   YOUR X-RDP INFO"
-echo "______________________"
+echo "========================"
+echo "    YOUR X-RDP INFO"
+echo "========================"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Username: bintang"
 echo "Password: zaquine"
-echo "______________________"
+echo "========================"
 echo ""
 echo ""
 echo "NOTE:"

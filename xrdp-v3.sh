ca6b75438912dab23e45726c76e580ee0ff8e137
Ryo Zaquine <92768269+zaquine@users.noreply.github.com>
2022-02-24 23:30:56 +0700

    Update xrdp-v3.sh

--- xrdp-v3.sh
+++ xrdp-v3.sh
@@ -1,5 +1,5 @@
 #Script V3
-#Auto Create X_RDP On Google Colab
+#Auto Create X-RDP On Google Colab
 #Firefox Installed
 #Chrome Installed
 #Created by: Bintang Zaquine
@@ -10,34 +10,34 @@ echo ""
 echo "PLEASE USE STABIL CONNECTION"
 echo ""
 echo ""
+echo "Progress Starting....."
 nohup ./ngrok tcp --region ap 3389 &>/dev/null &
 sudo apt install lxde > /dev/null 2>&1
 sudo apt install -y xrdp > /dev/null 2>&1
 sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
-echo "======================"
-echo "INSTALLING X-RDP DONE"
-echo "======================"
+echo "Info: INSTALLING X-RDP DONE"
 echo ""
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F > /dev/null 2>&1
 sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main" > /dev/null 2>&1
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get install firefox -y > /dev/null 2>&1
-echo "================-======"
-echo "INSTALLING FIREFOX DONE"
-echo "======================="
+echo "Info: INSTALLING FIREFOX SUCCESS"
 echo ""
-echo "============================"
-echo "WAIT FOR COMPLETING PROGRESS"
-echo "============================"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+sudo dpkg --install google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+sudo apt-get install --assume-yes --fix-broken > /dev/null 2>&1
+echo "Info: INSTALLING CHROME SUCCESS"
 echo ""
+echo "Info: FINISHING PROGRESS......."
 sudo service xrdp start > /dev/null 2>&1
-echo "======================"
+sudo clear
+echo "______________________"
 echo "   YOUR X-RDP INFO"
-echo "======================"
+echo "______________________"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Username: bintang"
 echo "Password: zaquine"
-echo "======================"
+echo "______________________"
 echo ""
 echo ""
 echo "NOTE: Don't close this tab to keep your RDP still running..."

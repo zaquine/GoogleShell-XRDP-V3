bc3fc182557133efe54c5f19d2f733828162e55d
Ryo Zaquine <92768269+zaquine@users.noreply.github.com>
2022-02-25 01:42:40 +0700

    Update build-xrdp-v3.sh

--- build-xrdp-v3.sh
+++ build-xrdp-v3.sh
@@ -3,15 +3,17 @@
 #Firefox Installed
 #Chrome Installed
 #Created by: Bintang Zaquine
+
 rm -fr google-xrdp-v2.sh
-echo "USE STABIL CONNECTION DURING INSTALATION"
+echo "---USE STABIL CONNECTION DURING INSTALATION---"
 echo ""
 echo "Progress Runing...Please Wait for 5 minute..."
 echo ""
+nohup ./ngrok tcp --region ap 3389 &>/dev/null &
 sudo apt install lxde > /dev/null 2>&1
 sudo apt install -y xrdp > /dev/null 2>&1
 sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
-echo "Info: INSTALLING X-RDP DONE"
+echo "[Info]: INSTALLING X-RDP DONE"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F > /dev/null 2>&1
 sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main" > /dev/null 2>&1
 sudo apt-get update > /dev/null 2>&1
@@ -24,7 +26,6 @@ echo "Info: INSTALLING CHROME SUCCESS"
 echo ""
 echo "[Info]: FINISHING PROGRESS......."
 echo ""
-nohup ./ngrok tcp --region ap 3389 &>/dev/null &
 sudo service xrdp start > /dev/null 2>&1
 echo "========================"
 echo "    YOUR X-RDP INFO"

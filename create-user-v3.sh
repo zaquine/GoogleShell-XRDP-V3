fea61eaac3098ceb172d9cddc8a4c12ef17210af
Ryo Zaquine <92768269+zaquine@users.noreply.github.com>
2022-02-24 23:19:34 +0700

    Create create-user-v3.sh

---
+++ create-user-v3.sh
@@ -0,0 +1,12 @@
+#Script V3
+#Auto Created User Login
+#Created by: Bintang Zaquine
+
+import os
+username = "bintang" #@param {type:"string"}
+password = "zaquine" #@param {type:"string"}
+os.system(f"useradd -m {username}")
+os.system(f"adduser {username} sudo")
+os.system(f"echo '{username}:{password}' | sudo chpasswd")
+os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
+print("User Login Created...")

1f9c183134ce941cb1d67e9140086fba6294aab3
Ryo Zaquine <92768269+zaquine@users.noreply.github.com>
2022-02-25 01:04:10 +0700

    Update build-xrdp-v3.sh

--- build-xrdp-v3.sh
+++ build-xrdp-v3.sh
@@ -5,30 +5,28 @@
 #Created by: Bintang Zaquine
 
 rm -fr google-xrdp-v2.sh
-echo "PROGRESS TIME : 5 MINUTE"
+echo "Info: PROGRESS TIME : 5 MINUTE"
+echo "Info: PLEASE USE STABIL CONNECTION"
 echo ""
-echo "PLEASE USE STABIL CONNECTION"
+echo "Running Progress....."
 echo ""
-echo ""
-echo "Progress Starting....."
 nohup ./ngrok tcp --region ap 3389 &>/dev/null &
 sudo apt install lxde > /dev/null 2>&1
 sudo apt install -y xrdp > /dev/null 2>&1
 sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
 echo "Info: INSTALLING X-RDP DONE"
-echo ""
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F > /dev/null 2>&1
 sudo apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main" > /dev/null 2>&1
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get install firefox -y > /dev/null 2>&1
 echo "Info: INSTALLING FIREFOX SUCCESS"
-echo ""
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
 sudo dpkg --install google-chrome-stable_current_amd64.deb > /dev/null 2>&1
 sudo apt-get install --assume-yes --fix-broken > /dev/null 2>&1
 echo "Info: INSTALLING CHROME SUCCESS"
 echo ""
 echo "Info: FINISHING PROGRESS......."
+echo ""
 sudo service xrdp start > /dev/null 2>&1
 echo "______________________"
 echo "   YOUR X-RDP INFO"
